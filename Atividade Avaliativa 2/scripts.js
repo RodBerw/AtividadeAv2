17f2c3242e648b984c11bf64c5606f0d760ca354
RodBerw <83656054+RodBerw@users.noreply.github.com>
2022-05-23 11:04:48 -0300

    commit 2

--- Atividade Avaliativa 2/scripts.js
+++ Atividade Avaliativa 2/scripts.js
@@ -21,3 +21,35 @@ function atividade2(){
         document.getElementById("resultado").innerHTML = `O peso ideal é: ${(62.1 * altura) - 44.7}.`;
     }
 }
+
+function atividade3(){
+    var v = [];
+    v[0] = Number(window.prompt("Digite o 1º valor inteiro: "));
+    v[1] = Number(window.prompt("Digite 0 2º valor inteiro: "));
+    v[2] = Number(window.prompt("Digite 0 3º valor inteiro: "));
+    var menor = v[0];
+
+    for(var i = 0; i < 3; i++){
+        if(v[i] < menor){
+            menor = v[i];
+        }
+    }
+
+    document.getElementById("resultado").innerHTML = `O menor dos valores é: ${menor}.`;
+}
+
+function atividade4(){
+    var n = Number(window.prompt("Digite um valor inteiro: "));
+
+    if(n >= 0){
+        if(n % 2 == 0){
+            document.getElementById("resultado").innerHTML = `O número ${n} é par.`;
+        }
+        else{
+            document.getElementById("resultado").innerHTML = `O número ${n} é ímpar.`;
+        }
+    }
+    else{
+        document.getElementById("resultado").innerHTML = `O valor absoluto do número ${n} é ${-n}`;
+    }
+}
